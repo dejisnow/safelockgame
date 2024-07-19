08063efd4c5cd6fd50cb7783d9b9e1fc11083dba
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-07-19 23:04:16 +0100

    Update file.js

--- file.js
+++ file.js
@@ -2,17 +2,31 @@ window.onload=()=>{
     
    //Lock key variables
    let keys =  document.querySelectorAll(".key")
+   
+   let lockBtn = document.querySelector(".lock")
+    let topLock = document.querySelector(".topLock")
+    
+    let array = [];
     
     keys.forEach((key)=>{
-  key.addEventListener("click",()=>{
-  if(colorArray.length >= 1 && colorArray[0] === colorArray[1]){
-  box.style.display= "none"
-      alert("You win")
-      location.reload()
-  }
+  key.addEventListener("click",(e)=>{
+  
+   array.push(e.target.innerHTML)
+     console.log(array)
+    
+  })})
+  
+  
+  lockBtn.addEventListener("click",()=>{
+      
+      topLock.classList.toggle("active")
+      topLock.style.top = "-5%"
+      
+      
+  })
     
     
     
     
     
-  }
+}
